11b9cf116747080109b2ae4d06fef34d42ad4366
Alexander <voratius@gmail.com>
2016-10-07 20:16:13 +0300

    Add files via upload

--- javascripts/_vti_cnf/main.js
+++ javascripts/_vti_cnf/main.js
@@ -1,4 +1,6 @@
 vti_encoding:SR|utf8-nl
 vti_timelastmodified:TR|26 Aug 2013 03:02:28 -0000
 vti_extenderversion:SR|6.0.2.8161
-vti_backlinkinfo:VX|test.html contact.html about.html faq.html 404.html index.html network.html
+vti_cacheddtm:TX|26 Aug 2013 03:02:28 -0000
+vti_filesize:IR|48
+vti_backlinkinfo:VX|
